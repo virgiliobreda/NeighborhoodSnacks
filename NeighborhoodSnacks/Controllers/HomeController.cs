520dcdcb10877df8da0a80f85abdedd132d13522
virgiliobreda <virgiliobred031@gmail.com>
2024-03-07 10:19:37 -0300

    Creating Project

---
+++ NeighborhoodSnacks/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NeighborhoodSnacks.Models;
+using System.Diagnostics;
+
+namespace NeighborhoodSnacks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NeighborhoodSnacks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NeighborhoodSnacks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
